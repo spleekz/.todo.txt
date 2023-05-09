357103ea1ca5e485851a852724253e7a3bca8160
spleekz <spleekz@zohomail.com>
2023-05-09 15:10:20 +0300

    change task markers

--- src/parse/task.js
+++ src/parse/task.js
@@ -1,5 +1,5 @@
-export const activeTaskMarker = '—'
-export const doneTaskMarker = '#'
+export const activeTaskMarker = '-'
+export const doneTaskMarker = '*'
 
 export const activeTaskRegex = new RegExp( `^\\${ activeTaskMarker }\t.*\n(((\t.*\n){1,})|)$`, 'gm' )
 export const doneTaskRegex = new RegExp( `^\t\\${ doneTaskMarker }\t.*\n(((\t{1,}.*\n){1,})|)$`, 'gm' )
